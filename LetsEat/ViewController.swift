07b87dda8c0db26cf111af940f9bf631a072f7db
Alfin Indrawan <alfinindrawan54@gmail.com>
2022-05-27 18:45:10 +0700

    init

---
+++ LetsEat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsEat
+//
+//  Created by Alfin on 27/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
