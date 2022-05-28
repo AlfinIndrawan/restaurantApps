777d6ec3cad7ee53660dc3666c4cfdd2e39b7bce
Alfin Indrawan <alfinindrawan54@gmail.com>
2022-05-28 15:53:01 +0700

    swiftlint

---
+++ LetsEat/Explore/ExploreViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by Alfin on 27/5/22.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    @IBOutlet var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
